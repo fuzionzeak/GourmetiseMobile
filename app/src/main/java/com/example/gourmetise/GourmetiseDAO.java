014f70183bd1c320fdb1e141886f9dcbaa454dc1
Mhoumadi Djaddoulhak <djaddou.youssouf@gmail.com>
2023-12-07 10:58:52 +0100

    ajout Helper + DAO

---
+++ app/src/main/java/com/example/gourmetise/GourmetiseDAO.java
@@ -0,0 +1,4 @@
+package com.example.gourmetise;
+
+public class GourmetiseDAO {
+}
--- app/src/main/java/com/example/gourmetise/MainActivity.java
+++ app/src/main/java/com/example/gourmetise/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gourmetise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
