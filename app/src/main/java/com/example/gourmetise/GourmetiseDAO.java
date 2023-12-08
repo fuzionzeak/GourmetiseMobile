4f9e62bc5291b395af8d420c61a18e52fa5bd1d1
Mhoumadi Djaddoulhak <djaddou.youssouf@gmail.com>
2023-12-08 13:23:38 +0100

    ajout Helper + DAO

--- app/src/main/java/com/example/gourmetise/GourmetiseDAO.java
+++ app/src/main/java/com/example/gourmetise/GourmetiseDAO.java
@@ -5,4 +5,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
 
 public class GourmetiseDAO {
+
+    private SQLiteDatabase maBase;
+    private GourmetiseHelper monHelper;
+
+    public GourmetiseDAO(Context context) {
+        monHelper = new GourmetiseHelper(context);
+        maBase = monHelper.getWritableDatabase();
+
+
+    }
 }
