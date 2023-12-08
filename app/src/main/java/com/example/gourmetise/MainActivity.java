f7f91844afb27d467d501c9aa465b86329ba444c
Anthony <fuzionzeak@gmail.com>
2023-12-08 13:17:57 +0100

    modif bde

--- app/src/main/java/com/example/gourmetise/MainActivity.java
+++ app/src/main/java/com/example/gourmetise/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GourmetiseDAO bdd;
+        bdd = new GourmetiseDAO(MainActivity.this);
     }
 
 
