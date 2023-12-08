c3f2f8a9dfc267b0481da8db222b67fdbe7c28fd
Anthony <fuzionzeak@gmail.com>
2023-12-08 14:44:48 +0100

    modif bde

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.gourmetise"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.gourmetise"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
