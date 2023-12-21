1297b35708eab02289fee5bf783a65d8ea4ca1a5
Mhoumadi Djaddoulhak <djaddou.youssouf@gmail.com>
2023-12-21 11:37:03 +0100

    ajout Helper + DAO

---
+++ app/src/main/java/com/example/gourmetise/AsyncHttpboClient.java
@@ -0,0 +1,8 @@
+package com.example.gourmetise;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+public class AsyncHttpboClient {
+    public void get(String urlI, JsonHttpResponseHandler jsonHttpResponseHandler) {
+    }
+}
