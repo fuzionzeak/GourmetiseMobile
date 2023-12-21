16f9c9ad61a5ca6d422beee325bc40ff48c199ba
Anthony <fuzionzeak@gmail.com>
2023-12-21 11:34:44 +0100

    avancement bdd embarquée

--- app/src/main/java/com/example/gourmetise/Boulangerie.java
+++ app/src/main/java/com/example/gourmetise/Boulangerie.java
@@ -25,14 +25,26 @@ public class Boulangerie {
     }
 
     public String getSiren() {return siren;}
+    public void setSiren(String siren) {this.siren = siren;}
+
 
     public String getNom() {return nom;}
+    public void setNom(String nom) {this.nom = nom;}
+
 
     public String getRue() {return rue;}
+    public void setRue(String rue) {this.rue = rue;}
+
+
     public String getVille() {return ville;}
+    public void setVille(String ville) {this.ville = ville;}
+
+
     public String getCode_postal() {return code_postal;}
+    public void setCode_postal(String code_postal) {this.code_postal = code_postal;}
+
+
     public String getDescriptif() {return descriptif;}
-
-
+    public void setDescriptif(String descriptif) {this.descriptif = descriptif;}
 }
 
